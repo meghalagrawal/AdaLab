5e16178c292fb55d27bd158898415b689f999191
Meghal <meghal.nit@gmail.com>
2017-01-17 16:19:41 +0530

    Tried but has bugs #Merge Sort in Exp1

--- Experiment1/merge_sort.cpp
+++ Experiment1/merge_sort.cpp
@@ -22,7 +22,7 @@ void merge_sort(int start,int end){
 
 void merge(int start,int mid,int end){
 
-	cout<<"Merge Start "<<start<<"Mid "<<mid<<"End "<<end<<endl;
+	cout<<"Merge Start "<<start<<" Mid "<<mid<<" End "<<end<<endl;
 
 int temp_array[50];
 int start1,mid1,i,k;
@@ -31,19 +31,18 @@ start1=start;
 i=start;
 mid1=mid+1;
 
-while((start1<=mid) && (mid1<=end)){
+while((start1<=mid) && (mid1<end)){
 
 	if (unsorted_array[start1]<=unsorted_array[mid1]){
 
 		temp_array[i]=unsorted_array[start1];
-		cout<<endl<<"Added "<<unsorted_array[start1]<<endl<<endl;
+		cout<<endl<<"Added "<<unsorted_array[start1]<<" In Index "<<i<<" Line 39 "<<endl<<endl;
 		start1++;
 
 	}else{
 
 		temp_array[i]=unsorted_array[mid1];
-		cout<<endl<<"Added "<<unsorted_array[mid1]<<endl<<endl;
-
+		cout<<endl<<"Added "<<unsorted_array[mid1]<<" In Index "<<i<<" Line 45 "<<endl<<endl;
 		mid1++;
 	}
 	i++;
@@ -55,17 +54,17 @@ if(start1>mid){
 	for(k=mid1;k<end;k++){
 
 		temp_array[i]=unsorted_array[k];
-		cout<<endl<<"Added "<<unsorted_array[k]<<endl<<endl;
+		cout<<endl<<"Added "<<unsorted_array[k]<<" In Index "<<i<<" Line 58 "<<endl<<endl;
 		
 		i++;
 	}
 
 }else{
 
-	for(k=start1;k<mid;k++){
+	for(k=start1;k<=mid;k++){
 
 		temp_array[i]=unsorted_array[k];
-		cout<<endl<<"Added "<<unsorted_array[k]<<endl<<endl;
+		cout<<endl<<"Added "<<unsorted_array[k]<<" In Index "<<i<<" Line 68 "<<endl<<endl;
 		i++;
 	}
 }
@@ -87,18 +86,18 @@ int main() {
 	cin>>number;
 	cout<<endl;
 	cout<<"Please Enter the  Elements to sort";
-	for(i=1;i<number+1;i++){
+	for(i=0;i<number;i++){
 
 		cin>>unsorted_array[i];
 
 	}
-	merge_sort(1,number+1);
+	merge_sort(0,number);
 	cout<<endl;
  	cout<<"Sorted List is below "<<endl;
 	cout<<endl<<endl;
 
 
-	for(i=1;i<number+1;i++){
+	for(i=0;i<number;i++){
 
 		cout<<unsorted_array[i]<<" ";
 	}
